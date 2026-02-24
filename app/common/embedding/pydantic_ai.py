238745590045530c219564f12fbfb87f73016f38
Chris Grimble <christopher.grimble@zego.com>
2026-02-24 15:35:05 +0000

    Use pydantic-ai to generate embedding rather than Boto3

---
+++ app/common/embedding/pydantic_ai.py
@@ -0,0 +1,27 @@
+from pydantic_ai import Embedder
+from pydantic_ai.embeddings.bedrock import (
+    BedrockEmbeddingModel,
+    BedrockEmbeddingSettings,
+)
+from pydantic_ai.providers.bedrock import BedrockProvider
+
+from app.common.embedding.service import AbstractEmbeddingService
+from app.config import AppConfig
+
+
+class PydanticAiEmbeddingService(AbstractEmbeddingService):
+    def __init__(self, config: AppConfig):
+        provider = BedrockProvider(
+            region_name=config.aws_region,
+        )
+        self._embedder = Embedder(
+            settings=BedrockEmbeddingSettings(dimensions=1024),
+            model=BedrockEmbeddingModel(
+                config.bedrock_embedding_config.model_id, provider=provider
+            ),
+        )
+
+    async def generate_embeddings(self, input_text: str) -> list[float]:
+        embedding_result = await self._embedder.embed_documents(input_text)
+
+        return list(embedding_result.embeddings[0])
